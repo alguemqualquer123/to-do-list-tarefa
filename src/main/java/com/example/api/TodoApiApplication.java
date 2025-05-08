7baed3adf3a2b5ae38becf617024e934b5d0438a
alguemqualquer123 <danilovinicius10337@gmail.com>
2025-05-08 17:34:28 -0300

    first commits

---
+++ src/main/java/com/example/api/TodoApiApplication.java
@@ -0,0 +1,29 @@
+package com.example.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/api")
+public class TodoApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApiApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public ResponseEntity<String> defaultRoute() {
+		String routes = "Olá Dev";
+		return ResponseEntity.ok(routes);
+	}
+
+
+
+
+
+
+}
